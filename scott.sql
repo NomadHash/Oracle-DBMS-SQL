db4c873bcf34246df0b5f966be1f0a85c7bcdbc4
DESKTOP-BP6VHSD\acorn <badsoap141@gmail.com>
2020-07-16 18:25:59 +0900

    16 정리

--- scott.sql
+++ scott.sql
@@ -231,10 +231,330 @@ TRIM(LEADING | TRAILING | BOTH + 문자열 + FROM 컬럼)*/
    SELECT MOD(3,2)
    FROM DUAL; --// 1
    
+   --ex1) 
+   SELECT SAL, MOD(SAL,30)
+   FROM EMP
+   ORDER BY SAL DESC;
+   
+   /* 4. ROUND function (n의 자리에서 반올림)
+    stntax // ROUND(column1,n) */
+   SELECT ROUND(1.1634,1) --//소숫점 첫째 자리 뒤 값으로 반올림.
+   FROM DUAL; --// 1.2
+   
+   /* 5. TRUNC function (소숫점 버림)
+      syntax TRUNC(column1,n)*/
+      SELECT TRUNC(456.789,2) -- 소숫점 기준으로 두칸까지 출력
+      FROM DUAL; --// 456.78
+      
+      ----------------------------------------
+      
+      -- Date, time function
+      SELECT SYSDATE -- 날짜
+      FROM DUAL;
+      
+      --ex1)
+      SELECT SYSDATE 날짜, SYSDATE +1 내일
+      FROM DUAL; --// 연산 가능
+      
+      --ex2)
+      SELECT ENAME 이름, TRUNC((SYSDATE-HIREDATE)/365)년차 
+      FROM EMP;
+      
+      SELECT SYSTIMESTAMP --날짜 및 시간
+      FROM DUAL;
+      
+      /*1. MONTHS_BETWEEN function
+      syntax// MONTHS_BETWEEN(data1,data2)
+            ex) MONTHS_BETWEEN(SYSDATE,HIREDATA)
+                data1과 data2 사이의 시간 */
+    
+    --ex1)          
+    SELECT ENAME, HIREDATE,
+    TRUNC(MONTHS_BETWEEN(SYSDATE,HIREDATE),0) T_BET
+    FROM EMP
+    WHERE DEPTNO = 10
+    ORDER BY MONTHS_BETWEEN(SYSDATE,HIREDATE) DESC;
+    
+    --ex2)
+    SELECT ENAME, HIREDATE, ADD_MONTHS(HIREDATE,-5) A_MON
+    FROM EMP
+    WHERE DEPTNO = 10
+    ORDER BY ADD_MONTHS(HIREDATE,5) DESC;
+    
+    --ex3)
+    SELECT ENAME 이름, HIREDATE 입사일,
+    TRUNC(SYSDATE-HIREDATE,0)"전체 일자",
+    TRUNC((SYSDATE-HIREDATE)/7,0) 주,
+    ROUND(MOD((SYSDATE - HIREDATE),7),0) DAYS
+    FROM EMP
+    ORDER BY SYSDATE-HIREDATE DESC;
+    /* SYSDATE-HIREDATE = 현재 날짜로부터 
+    해당 날짜 까지의 일 */
+    
+   
+SELECT TO_CHAR(SYSDATE, 'YYYY/MM/DD,(AM)DY HH24:MI:SS'),
+    'YYYY/MM/DD,(AM)DY HH24:MI:SS'
+    FROM DUAL;
+
+SELECT ENAME 이름, HIREDATE 입사일, SAL 급여
+FROM EMP
+WHERE TO_CHAR(HIREDATE,'MM')='09'; --//9월에 입사한 사람 탐색
+
+
+SELECT ENAME 이름, HIREDATE 입사일, SAL 급여
+FROM EMP
+WHERE TO_CHAR(HIREDATE, 'YY')='81'--//81년도에 입사한 사람 탐색
+ORDER BY TO_CHAR(HIREDATE, 'MM');
+
+--EMP 테이블의 10번 부서중 입사일자를 '~년 ~월 ~일 로 출력
+
+SELECT ENAME,HIREDATE, TO_CHAR(HIREDATE,'YYYY"년" MM"월" DD"일"')입사일
+FROM EMP
+WHERE DEPTNO =10;
+
+
+SELECT ENAME, JOB,SAL, TO_CHAR(SAL,'$999,999')
+FROM EMP
+WHERE DEPTNO =20
+ORDER BY SAL DESC;
+
+--TO NUMBER
+SELECT TO_NUMBER('1234'+100)
+FROM DUAL;
+
+
+
+/* CASE function 
+   해당 컬럼을 기준으로 새로운 컬럼 생성*/
+   
+-- ex1)
+SELECT EMPNO, ENAME, JOB, SAL "전 급여",
+CASE JOB WHEN 'ANALYST' THEN SAL * 1.1
+         WHEN 'CLERK' THEN SAL * 1.2
+         WHEN 'MANAGER' THEN SAL * 1.3
+         WHEN 'PRESIDENT' THEN SAL * 1.4
+         WHEN 'SALESMAN' THEN SAL * 1.5
+END "후 급여"
+FROM EMP;
+
+-- ex)2
+SELECT EMPNO 사원번호, ENAME 이름, JOB 직책, SAL 급여,
+CASE WHEN SAL >=0 AND SAL <=1000 THEN 'E'
+     WHEN SAL >1000 AND SAL <=2000 THEN 'D' 
+     WHEN SAL >2000 AND SAL <=3000 THEN 'C' 
+     WHEN SAL >3000 AND SAL <=4000 THEN 'B' 
+     WHEN SAL >4000 AND SAL <=5000 THEN 'A'
+END 등급
+FROM EMP
+ORDER BY 등급;
+
+/* DECODE function */
+   SELECT EMPNO, ENAME, JOB, SAL 급여,
+     DECODE(JOB, 'ANALYST' , SAL*1.1,
+                 'CLERK' , SAL*1.2,
+                 'MANAGER' , SAL*1.3,
+                 'PRESIDENT' , SAL*1.4,
+                 'SALESMAN' , SAL*1.5, SAL) "후 급여"
+   FROM EMP;
+   
+   /*SIGN function //주어진 숫자가 양수인지 음수인지 0인지 구하는 함수
+   
+   --=== 실습 문제 =====================================================
+   
+   /* 1. 사원테이블에서 입사일이 12월인 사원의 사번, 사원명, 입사일 검색하시오 */
+   SELECT EMPNO 사번, ENAME 사원명, HIREDATE 입사일
+   FROM EMP
+   WHERE TO_CHAR(HIREDATE, 'MM')='12';
+   
+   /* 2. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오.*/
+   SELECT EMPNO, ENAME, LPAD(SAL,10,'*') "급여"
+   FROM EMP;
+   
+   /* 3. 다음과 같은 결과를 검색할수있는 SQL 문장을 작성하시오.*/
+   
+
+   SELECT EMPNO, ENAME, COMM, 
+   CASE WHEN COMM IS NULL THEN '일반사원'
+        ELSE '영업사원'
+   END "사원 종류"
+   FROM EMP;
+   
+    SELECT LOC,
+     DECODE(LOC, 'NEW YORK' , 'EAST', 
+                 'DALLAS', 'CENTER',
+                 'CHICAGO', 'CENTER',
+                 'BOSTON', 'EAST',
+                 LOC) AREA
+   FROM DEPT;
+   
+   
+    SELECT EMPNO, ENAME, JOB, SAL 급여,
+     DECODE(JOB, 'ANALYST' , SAL*1.1,
+                 'CLERK' , SAL*1.2,
+                 'MANAGER' , SAL*1.3,
+                 'PRESIDENT' , SAL*1.4,
+                 'SALESMAN' , SAL*1.5, SAL) "후 급여"
+    FROM EMP;
+   
+   
+   SELECT STUDENT_NO,STUDENT_NAME, STUDENT_SSN
+   FROM TB_STUDENT
+   WHERE STUDENT_ADDRESS LIKE '전주%'
+   AND ABSENCE_YN = 'N'
+   AND STUDENT_NO LIKE 'A2%';
+   
+   
+   -- Group Function
+   SELECT SUM(SAL), ROUND(AVG(SAL)), MAX(SAL), MIN(SAL)
+   FROM EMP;
+   
+   SELECT MAX(HIREDATE), MIN(HIREDATE)
+   FROM EMP;
+   
+   SELECT COUNT(ENAME), COUNT(COMM)
+   FROM EMP;
+   
+   SELECT COUNT(JOB), COUNT(DISTINCT JOB)
+   FROM EMP;
+   
+   SELECT COUNT(*)
+   FROM EMP;
+   
+   SELECT AVG(SAL), MAX(SAL), MIN(SAL), SUM(SAL)
+   FROM EMP
+   WHERE JOB LIKE 'SAL%';
+   
+ 
+  /* GROUP BY 
+      전체 테이블이 아닌 특정 그룹으로 묶을 때 사용. */
+      --WHERE 절에는 그룹 함수가 올 수 없다.
+      
+      SELECT DEPTNO, AVG(SAL)
+      FROM EMP
+      GROUP BY DEPTNO;
+      -- DEPTNO의 항목별로 SAL의 평균값을 출력.
+      
+      SELECT DEPTNO, COUNT(*), ROUND(AVG(SAL)),MIN(SAL),MAX(SAL)
+      FROM EMP
+      GROUP BY DEPTNO;
+      
+        SELECT DEPTNO, COUNT(*), ROUND(AVG(SAL)),MIN(SAL),MAX(SAL)
+        , SUM(SAL)
+      FROM EMP
+      GROUP BY DEPTNO
+      ORDER BY SUM(SAL) DESC;
+      
+      SELECT DEPTNO, JOB, COUNT(*), AVG(SAL),SUM(SAL)
+      FROM EMP
+      GROUP BY DEPTNO, JOB;
+      
+     -- HAVING //GROUP BY의 조건식
+     
+     SELECT DEPTNO, SUM(SAL)
+     FROM EMP
+     GROUP BY DEPTNO
+     HAVING SUM(SAL) >9000;
+     
+     SELECT DEPTNO, SUM(SAL)
+     FROM EMP
+     WHERE SAL >800
+     GROUP BY DEPTNO
+     HAVING SUM(SAL) >9000;
+     
+     SELECT DEPTNO, AVG(SAL), SUM(SAL)
+     FROM EMP
+     GROUP BY DEPTNO
+     HAVING MAX(SAL)> 2900;
+     
+     SELECT JOB, AVG(SAL), SUM(SAL)
+     FROM EMP
+     GROUP BY JOB
+     HAVING AVG(SAL) >=3000;
+     
+     SELECT JOB, SUM(SAL) PAYROLL
+     FROM EMP
+     WHERE JOB NOT LIKE 'SALE%'
+     GROUP BY JOB
+     HAVING SUM(SAL) > 5000
+     ORDER BY SUM(SAL) DESC;
+     
+     SELECT DEPTNO , AVG(SAL)
+     FROM EMP
+     GROUP BY DEPTNO;
+     
+     SELECT DEPTNO, COUNT(DEPTNO)
+     FROM EMP
+     GROUP BY DEPTNO
+     HAVING COUNT(DEPTNO) >= 6;
+     
+     SELECT DEPTNO, MAX(SAL), JOB 
+     FROM EMP
+     GROUP BY DEPTNO,JOB
+     HAVING MAX(SAL)>=5000;
+     
+     
+     SELECT TO_CHAR(HIREDATE,'YYYY') 년, TO_CHAR(HIREDATE,'MM') 월, SUM(SAL)
+     FROM EMP
+     GROUP BY TO_CHAR(HIREDATE,'YYYY'), TO_CHAR(HIREDATE,'MM')
+     ORDER BY 년;
+     
+     
+     SELECT COUNT(DEPARTMENT_NO) 학생수
+     FROM TB_STUDENT
+     WHERE ENTRANCE_DATE LIKE '01%'
+     AND DEPARTMENT_NO = 003;
+    
+    
+    SELECT CATEGORY 계열, DEPARTMENT_NAME 학과이름, CAPACITY 정원
+    FROM TB_DEPARTMENT
+    WHERE CATEGORY = '공학'
+    AND CAPACITY BETWEEN 20 AND 30
+    ORDER BY DEPARTMENT_NAME ASC;
+    
+    
+    SELECT CATEGORY 계열, COUNT(CATEGORY) 학과수 
+      FROM TB_DEPARTMENT
+      GROUP BY CATEGORY
+      HAVING CATEGORY LIKE '%학%'
+      ORDER BY 학과수 DESC;
+      
+
+    
+    
+    
+     
+     
+     
+     
+    
+     
+    
+    aabbcc
+    for(let i=1; i<s.length/2; i++){
+    s.substr(i,i)
+    }
+    
+ 
    
    
    
    
+
+   
+
+
+
+  
+  
+  
+   
+   
+   
+   
+   
+   
+   
+
    
   
    
